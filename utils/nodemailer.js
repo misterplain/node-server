24c39f9bcabf64302b671571198ad3569d4b35f4
Patrick <obrienp2321@gmail.com>
2025-10-28 23:49:00 +0100

    clean nodemailer logic and flow

--- utils/nodemailer.js
+++ utils/nodemailer.js
@@ -27,15 +27,14 @@ function nodeMailerConfirmationEmail(source, messageData) {
      `,
   };
 
-  smtpTransporter.sendMail(mailOptions, (error) => {
-    try {
-      if (error)
-        return res.status(400).json({ msg: "Please Fill All The Fields!" });
-      res.status(200).json({ message: "Message sent successfully" });
-    } catch (error) {
-      if (error)
-        return res.status(500).json({ message: "Error sending message" });
-    }
+  return new Promise((resolve, reject) => {
+    smtpTransporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(info);
+      }
+    });
   });
 }
 
@@ -43,15 +42,14 @@ router.post("/", async (req, res) => {
   const { result, name, email, message, phoneNum } = req.body;
   const { source } = req.query;
 
-  try {
-    const emailSent = await nodeMailerConfirmationEmail(source, req.body);
-    return res
-      .status(200)
-      .json({ message: "Message sent successfully" + emailSent });
-  } catch (error) {
-    nodeMailerConfirmationEmail(source, { message: "Failure to send message" });
-    return res.status(500).json({ message: "Error sending message" });
-  }
+  nodeMailerConfirmationEmail(source, req.body)
+    .then(() => {
+      return res.status(200).json({ message: "Message sent successfully" });
+    })
+    .catch(async (err) => {
+      await nodeMailerConfirmationEmail(source, { message: "Failure to send message" });
+      return res.status(500).json({ message: "Error sending message" });
+    });
 });
 module.exports = router;
 
